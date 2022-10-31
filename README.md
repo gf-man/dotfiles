92f85e429135130fe0cf184a13feffb2ec5bd290
gf-man <georgefowkes94@gmail.com>
2022-10-31 15:09:24 +0000

    Readme and configs updated

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ My dotfiles and themes for linux.
 ### Installation
 1. cd into your $HOME directory
 2. git clone the url: https://github.com/gf-man/dotfiles.git
-3. run the install.sh to creat symlinks to the dotfiles (you will need to delete, rename or move your current/default config files)
+3. run the install.sh to creat symlinks to the dotfiles (new folders won't be created)
+4. for YCM cmake is required
--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/Xresources ~/.Xresources
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/Xresources ~/.Xresources
 
-ln -s ~/dotfiles/i3config ~/.config/i3/config
-ln -s ~/dotfiles/config.rasi ~/.config/rofi/config.rasi
-ln -s ~/dotfiles/theme.rasi ~/.config/rofi/theme.rasi
+ln -sf ~/dotfiles/i3config ~/.config/i3/config
+ln -sf ~/dotfiles/config.rasi ~/.config/rofi/config.rasi
+ln -sf ~/dotfiles/theme.rasi ~/.config/rofi/theme.rasi
 
-ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vimrc ~/.vimrc
 
