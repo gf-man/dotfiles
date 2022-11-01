087ccf937233dacabc1ebe7c00cab0ebafd44018
gf-man <georgefowkes94@gmail.com>
2022-11-01 18:38:50 +0000

    Updated i3config and added i3blocks config

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/Xresources ~/.Xresources
 
 ln -sf ~/dotfiles/i3config ~/.config/i3/config
+ln -sf ~/dotfiles/i3blocks.conf ~/.i3blocks.conf
+
 ln -sf ~/dotfiles/config.rasi ~/.config/rofi/config.rasi
 ln -sf ~/dotfiles/theme.rasi ~/.config/rofi/theme.rasi
 
